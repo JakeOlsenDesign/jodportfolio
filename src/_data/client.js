862db4a229eae5becc712596141e099bd51d27ee
JakeOlsenDesign <jakemckeeolsen@gmail.com>
2024-11-29 01:25:43 -0800

    new template

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Jake Olsen Design",
+    "email": "jakemckeeolsen@gmail.com",
+    "phoneForTel": "503-807-8834",
+    "phoneFormatted": "(503) 807-8834",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "7691 SW Hansen Ln",
+        "lineTwo": "",
+        "city": "Tigard",
+        "state": "OR",
+        "zip": "97224",
         "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "mapLink": "https://maps.app.goo.gl/BoVn7WhxYnDCjr2K6"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        // "facebook": "https://www.facebook.com/",
+        // "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.jakeolsendesign.com"
 };
