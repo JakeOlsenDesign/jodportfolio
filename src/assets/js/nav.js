6d5b035f1e1f03df4539af2960e3cb060402f21c
JakeOlsenDesign <jakemckeeolsen@gmail.com>
2025-01-20 23:04:51 -0800

    Update nav.js

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -126,7 +126,7 @@ window.addEventListener('scroll', function() {
   
     document.querySelectorAll('.blur').forEach(function(element) {
       var elementHeight = element.offsetHeight;
-      var opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 1;
+      var opacity = ((1 - (elementHeight - scrollTop) / elementHeight) / 0.8) + 1;
       element.style.opacity = opacity;
     //   var opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
     //   element.style.opacity = opacity;
