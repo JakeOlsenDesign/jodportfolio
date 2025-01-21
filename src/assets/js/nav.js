8aae9506c6f1d191f21c57ab4d1969ca8056d0a9
JakeOlsenDesign <jakemckeeolsen@gmail.com>
2025-01-20 23:17:36 -0800

    update

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -126,7 +126,7 @@ window.addEventListener('scroll', function() {
   
     document.querySelectorAll('.blur').forEach(function(element) {
       var elementHeight = element.offsetHeight;
-      var opacity = ((0 - (elementHeight - scrollTop) / elementHeight) * 0);
+      var opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
       element.style.opacity = opacity;
     });
   });
