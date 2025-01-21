d143c8e8ebdd4e43a6c62e6f34db1338150a6906
JakeOlsenDesign <jakemckeeolsen@gmail.com>
2025-01-20 23:37:36 -0800

    update

--- src/assets/js/scroll.js
+++ src/assets/js/scroll.js
@@ -1,9 +1,9 @@
-window.addEventListener('scroll', function() {
-    var scrollTop = window.scrollY; // Get the current vertical scroll position
+// window.addEventListener('scroll', function() {
+//     var scrollTop = window.scrollY; // Get the current vertical scroll position
   
-    document.querySelectorAll('.blur').forEach(function(element) {
-      var elementHeight = element.offsetHeight;
-      var opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
-      element.style.opacity = opacity;
-    });
-  });
+//     document.querySelectorAll('.blur').forEach(function(element) {
+//       var elementHeight = element.offsetHeight;
+//       var opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
+//       element.style.opacity = opacity;
+//     });
+//   });
