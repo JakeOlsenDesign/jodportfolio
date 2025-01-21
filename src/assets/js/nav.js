583ac864120acd30a4b48b9aa2b2d1055e5824dd
JakeOlsenDesign <jakemckeeolsen@gmail.com>
2025-01-21 00:16:13 -0800

    update

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -124,7 +124,7 @@ document.getElementById('main').style.marginTop = navbarHeight + 'px';
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', function() {
       let scroll = window.scrollY; // Get the current vertical scroll position
-      let opacity = 1 - (scroll / 3000);
+      let opacity = 1 - (scroll / 500);
       if (opacity >= 0) {
         document.querySelectorAll('.blur').forEach(function(element) {
           element.style.opacity = opacity;
