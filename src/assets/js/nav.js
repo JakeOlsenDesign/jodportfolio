31a4454e737b0618d57057da05c2898fa49aca66
JakeOlsenDesign <jakemckeeolsen@gmail.com>
2025-01-27 11:03:49 -0800

    up

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -183,6 +183,6 @@ function wrapWordsInSpansForSpecificClass(targetClass) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    wrapWordsInSpansForSpecificClass('service');
+    wrapWordsInSpansForSpecificClass('services');
   });
   
